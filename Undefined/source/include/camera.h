1a7484e25650ef6cba59c96b85fb12c0392e6555
Paul Bussy <p.bussy@student.isartdigital.com>
2024-02-16 09:16:26 +0100

    merge

---
+++ Undefined/source/include/camera.h
@@ -0,0 +1,46 @@
+#pragma once
+
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+#include "Matrix4x4.h"
+#include "Vector3.h"
+
+class Camera
+{
+public:
+    Camera(float width, float height);
+    ~Camera() = default;
+
+    Matrix4x4 ProjectionMatrix(float fovY, float aspect, float far, float near);
+    Matrix4x4 ViewMatrix(const Vector3& up, const Vector3& eye, const Vector3& lookAt);
+
+    const Matrix4x4& GetVP();
+
+    void Update();
+
+    void ProcessInput(GLFWwindow* window);
+
+    Vector3 Eye;
+    Vector3 LookAt;
+    Vector3 Up;
+
+    float Yaw;
+    float Pitch;
+
+    bool FirstMouse;
+    float LastX;
+    float LastY;
+
+    bool IsMouseForCam;
+
+private:
+    Matrix4x4 m_VP;
+
+    Matrix4x4 View;
+    Matrix4x4 Perspective;
+
+    float Width;
+    float Height;
+};
+
