c01f0cd17f45177dee75e1be97c535cf173a3693
Paul Bussy <p.bussy@student.isartdigital.com>
2024-04-12 14:41:56 +0200

    [update] did documentation (finish Resource folder)

--- undefined/source/include/resources/resource_manager.h
+++ undefined/source/include/resources/resource_manager.h
@@ -53,13 +53,33 @@ public:
 		return std::dynamic_pointer_cast<Resource>(p->second);
 	}
 
+	/// <summary>
+	/// Rename a Resource
+	/// </summary>
+	/// <param name="oldName">: Old name of the Resource you want to rename</param>
+	/// <param name="newName">: New name of the Resource</param>
 	UNDEFINED_ENGINE static void Rename(const std::string& oldName, const std::string& newName);
+	/// <summary>
+	/// Rename a folder
+	/// </summary>
+	/// <param name="oldName">: Old name of the folder you want to rename</param>
+	/// <param name="newName">: New name of the folder</param>
 	UNDEFINED_ENGINE static void RenameFolder(const std::string& oldName, const std::string& newName);
 
+	/// <summary>
+	/// Unload a Resource
+	/// </summary>
+	/// <param name="name">: Name of the Resource</param>
 	UNDEFINED_ENGINE static void Unload(const std::string& name);
+	/// <summary>
+	/// Unload all the Resource stored in the Resource Manager
+	/// </summary>
 	UNDEFINED_ENGINE static void UnloadAll();
 
 private:
 	UNDEFINED_ENGINE static inline std::vector<std::string> mShader;
+	/// <summary>
+	/// std::unordered_map to store the Resource from the Resource Manager
+	/// </summary>
 	UNDEFINED_ENGINE static inline std::unordered_map<std::string, std::shared_ptr<Resource>> mResources;
 };
--- undefined/source/include/resources/texture.h
+++ undefined/source/include/resources/texture.h
@@ -9,27 +9,84 @@
 #include "service_locator.h"
 #include "utils/flag.h"
 
+/// <summary>
+/// A Class to store all the Texture data
+/// </summary>
 class UNDEFINED_ENGINE Texture : public Resource
 {
 public:
+	/// <summary>
+	/// A Constructor of Texture, creating a blank texture with a width and height given
+	/// </summary>
+	/// <param name="width">: Width of the texture created</param>
+	/// <param name="height">: Height of the texture created</param>
 	Texture(unsigned int width, unsigned int height);
+	/// <summary>
+	/// A Constructor of Texture, creating a texture with an image (e.g : png or jpg)
+	/// </summary>
+	/// <param name="filepath">: Path of the image you want to create a texture</param>
+	/// <param name="isFlipped">: Do we need to flip the texture (by default : false)</param>
 	Texture(const char* filepath, bool isFlipped = false);
+	/// <summary>
+	/// Destructor of Texture
+	/// </summary>
 	~Texture();
 
+	/// <summary>
+	/// Get the Texture ID
+	/// </summary>
+	/// <returns>Return the Texture ID</returns>
 	unsigned int GetID() const;
+	/// <summary>
+	/// Set the Texture ID 
+	/// </summary>
+	/// <param name="newID">: New ID of the Texture</param>
 	void SetID(unsigned int newID);
+	/// <summary>
+	/// Get the Texture width
+	/// </summary>
+	/// <returns>Return the Texture width</returns>
 	unsigned int GetWidth() const;
+	/// <summary>
+	/// Get the Texture height
+	/// </summary>
+	/// <returns>Return the Texture height</returns>
 	unsigned int GetHeight() const;
+	/// <summary>
+	/// Check if the Resource is valid
+	/// </summary>
+	/// <returns>Return either true if it is valid or false</returns>
 	bool IsValid() const;
 
+	/// <summary>
+	/// Pointer for the Texture data
+	/// </summary>
 	const void* Data;
 
 private:
+	/// <summary>
+	/// ID of the Texture
+	/// </summary>
 	unsigned int mID = 0;
+	/// <summary>
+	/// Width of the Texture
+	/// </summary>
 	int mWidth = 0;
+	/// <summary>
+	/// Height of the Texture
+	/// </summary>
 	int mHeight = 0;
+
+	/// <summary>
+	/// Pointer to our Renderer to simplify the calls from the ServiceLocator
+	/// </summary>
 	Renderer* mRenderer = nullptr;
 
 public:
+	/// <summary>
+	/// Load a Cubemap
+	/// </summary>
+	/// <param name="faces">: std::vector of the faces of the Cubemap</param>
+	/// <returns>Return the ID of the Cubemap</returns>
 	static unsigned int LoadCubeMap(const std::vector<std::string>& faces);
 };
