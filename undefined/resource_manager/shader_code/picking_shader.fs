a6d3e9dcd894062d3ad4a9f6673e1422c0e85c45
Cyril KHALIL <c.khalil@student.isartdigital.com>
2024-05-23 17:31:28 +0200

    [remove] unused variables in shader

--- undefined/resource_manager/shader_code/picking_shader.fs
+++ undefined/resource_manager/shader_code/picking_shader.fs
@@ -1,17 +1,12 @@
 #version 450 core
 
 layout (location = 0) out vec4 FragColor;
-layout (location = 1) out int FragColor2;
 
 in vec3 FragPos;
 in vec2 TexCoord;
 in vec3 Normal;
 
-uniform int EntityID;
-
-
 void main()
 {
     FragColor = vec4(1.0, 0.0, 0.0, 1.0);
-    FragColor2 = EntityID;
 }
